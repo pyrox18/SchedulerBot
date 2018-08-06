9835de54be64ef4bf752473a804bcb8c5de51dd6
Haryz Izzudin <haryzizzudin@gmail.com>
2018-08-06 12:54:10 +0800

    Create migration tool project

---
+++ SchedulerBot.MigrationTool/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SchedulerBot.MigrationTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SchedulerBot Database Migration Tool (v1.0.x to v2.0.0)");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
