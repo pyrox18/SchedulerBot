fb861fe9dc591852426b843b35fe0559b62b844f
Haryz Izzudin <haryzizzudin@gmail.com>
2018-08-13 16:24:20 +0800

    Add null checking for event mentions in notify job

--- SchedulerBot.Client/Scheduler/EventNotifyJob.cs
+++ SchedulerBot.Client/Scheduler/EventNotifyJob.cs
@@ -21,29 +21,35 @@ namespace SchedulerBot.Client.Scheduler
             var channel = (DiscordChannel)jobDataMap["channel"];
 
             var embed = EventEmbedFactory.GetNotifyEventEmbed(evt);
-            StringBuilder sb = new StringBuilder();
-            foreach (var mention in evt.Mentions)
+            if (evt.Mentions != null)
             {
-                switch (mention.Type)
+                StringBuilder sb = new StringBuilder();
+                foreach (var mention in evt.Mentions)
                 {
-                    case Data.Models.MentionType.Role:
-                        sb.Append($"{mention.TargetId.AsRoleMention()} ");
-                        break;
-                    case Data.Models.MentionType.User:
-                        sb.Append($"{mention.TargetId.AsUserMention()} ");
-                        break;
-                    case Data.Models.MentionType.Everyone:
-                        sb.Append("@everyone");
-                        break;
-                    case Data.Models.MentionType.RSVP:
-                        foreach (var rsvp in evt.RSVPs)
-                        {
-                            sb.Append($"{rsvp.UserId.AsUserMention()} ");
-                        }
-                        break;
+                    switch (mention.Type)
+                    {
+                        case Data.Models.MentionType.Role:
+                            sb.Append($"{mention.TargetId.AsRoleMention()} ");
+                            break;
+                        case Data.Models.MentionType.User:
+                            sb.Append($"{mention.TargetId.AsUserMention()} ");
+                            break;
+                        case Data.Models.MentionType.Everyone:
+                            sb.Append("@everyone");
+                            break;
+                        case Data.Models.MentionType.RSVP:
+                            foreach (var rsvp in evt.RSVPs)
+                            {
+                                sb.Append($"{rsvp.UserId.AsUserMention()} ");
+                            }
+                            break;
+                    }
                 }
+                await client.SendMessageAsync(channel, sb.ToString(), embed: embed);
+                return;
             }
-            await client.SendMessageAsync(channel, sb.ToString(), embed: embed);
+
+            await client.SendMessageAsync(channel, embed: embed);
         }
     }
 }
