db78bb94e306b9ab95e7f065747c3e540023b11a
Haryz Izzudin <haryzizzudin@gmail.com>
2018-09-02 00:50:50 +0800

    Implement locking for initialiser command

--- SchedulerBot.Client/Commands/InitializerCommands.cs
+++ SchedulerBot.Client/Commands/InitializerCommands.cs
@@ -1,7 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using Microsoft.Extensions.Configuration;
+using RedLockNet;
+using SchedulerBot.Client.Exceptions;
 using SchedulerBot.Data.Services;
 
 namespace SchedulerBot.Client.Commands
@@ -10,11 +13,13 @@ namespace SchedulerBot.Client.Commands
     {
         private readonly ICalendarService _calendarService;
         private readonly IConfigurationRoot _configuration;
+        private readonly IDistributedLockFactory _redlockFactory;
 
-        public InitializerCommands(ICalendarService calendarService, IConfigurationRoot configuration)
+        public InitializerCommands(ICalendarService calendarService, IConfigurationRoot configuration, IDistributedLockFactory redlockFactory)
         {
             _calendarService = calendarService;
             _configuration = configuration;
+            _redlockFactory = redlockFactory;
         }
 
         [Command("init"), Description("Initialize the bot with a timezone and a default channel.")]
@@ -22,7 +27,19 @@ namespace SchedulerBot.Client.Commands
         {
             await ctx.TriggerTypingAsync();
 
-            var initSuccess = await _calendarService.InitialiseCalendar(ctx.Guild.Id, timezone, ctx.Channel.Id);
+            bool? initSuccess;
+            using (var redlock = await _redlockFactory.CreateLockAsync(ctx.Guild.Id.ToString(), TimeSpan.FromSeconds(15), TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(0.5)))
+            {
+                if (redlock.IsAcquired)
+                {
+                    initSuccess = await _calendarService.InitialiseCalendar(ctx.Guild.Id, timezone, ctx.Channel.Id);
+                }
+                else
+                {
+                    throw new RedisLockAcquireException($"Cannot acquire lock for guild {ctx.Guild.Id}");
+                }
+            }
+
             if (initSuccess == null)
             {
                 await ctx.RespondAsync("Timezone already initialised.");
