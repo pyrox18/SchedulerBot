8613b02ff41926341255be09b6698163a7dbc291
Haryz Izzudin <haryzizzudin@gmail.com>
2018-11-01 22:57:55 +0800

    Add tests for ulong extension methods

---
+++ SchedulerBot.Client.UnitTests/UlongExtensionsFacts.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using SchedulerBot.Client.Extensions;
+
+namespace SchedulerBot.Client.UnitTests
+{
+    public class UlongExtensionsFacts
+    {
+        public class AsChannelMentionMethod
+        {
+            [Fact]
+            public void ReturnsChannelMentionString()
+            {
+                ulong channelId = 12345;
+
+                var result = channelId.AsChannelMention();
+
+                Assert.Equal("<#12345>", result);
+            }
+
+            [Fact]
+            public void ReturnsUserMentionString()
+            {
+                ulong userId = 12345;
+
+                var result = userId.AsUserMention();
+
+                Assert.Equal("<@12345>", result);
+            }
+
+            [Fact]
+            public void ReturnsRoleMentionString()
+            {
+                ulong roleId = 12345;
+
+                var result = roleId.AsRoleMention();
+
+                Assert.Equal("<@&12345>", result);
+            }
+        }
+    }
+}
