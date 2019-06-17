53fa54e5565df427104d37634450fe83015a1b4e
Haryz Izzudin <haryzizzudin@gmail.com>
2019-06-17 08:26:39 +0800

    Create application layer with repository

---
+++ SchedulerBot.Application/Interfaces/IAsyncRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SchedulerBot.Application.Interfaces
+{
+    public interface IAsyncRepository<T>
+    {
+        Task<T> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<int> CountAsync(ISpecification<T> spec);
+    }
+}
