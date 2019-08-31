ba73ea5c6cef282d156387154f4221a6c88db2bf
Haryz Izzudin <haryzizzudin@gmail.com>
2019-08-31 18:29:16 +0800

    Add calendar repository implementation

---
+++ SchedulerBot.Persistence.UnitTests/Repositories/CalendarRepositoryFacts.cs
@@ -0,0 +1,242 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchedulerBot.Data.Models;
+using SchedulerBot.Persistence.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SchedulerBot.Persistence.UnitTests.Repositories
+{
+    public class CalendarRepositoryFacts
+    {
+        public class AddAsyncMethod
+        {
+            [Fact]
+            public async Task AddsCalendar()
+            {
+                var options = new DbContextOptionsBuilder<SchedulerBotDbContext>()
+                    .UseInMemoryDatabase(databaseName: $"{GetType().DeclaringType.Name}_{GetType().Name}_{nameof(AddsCalendar)}")
+                    .Options;
+
+                var calendar = new Calendar
+                {
+                    Id = 1
+                };
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    var repository = new CalendarRepository(context);
+
+                    var result = await repository.AddAsync(calendar);
+
+                    Assert.Equal(calendar.Id, result.Id);
+                }
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    Assert.Single(context.Calendars);
+                }
+            }
+        }
+
+        public class DeleteAllEventsAsyncMethod
+        {
+            [Fact]
+            public async Task DeletesAllEvents()
+            {
+                var options = new DbContextOptionsBuilder<SchedulerBotDbContext>()
+                    .UseInMemoryDatabase(databaseName: $"{GetType().DeclaringType.Name}_{GetType().Name}_{nameof(DeletesAllEvents)}")
+                    .Options;
+
+                var calendar = new Calendar
+                {
+                    Id = 1,
+                    Events = new List<Event>
+                    {
+                        new Event
+                        {
+                            Name = "Test Event",
+                            Mentions = new List<EventMention>
+                            {
+                                new EventMention
+                                {
+                                    TargetId = 2,
+                                    Type = MentionType.User
+                                }
+                            },
+                            RSVPs = new List<EventRSVP>
+                            {
+                                new EventRSVP
+                                {
+                                    UserId = 2
+                                }
+                            }
+                        }
+                    }
+                };
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    await context.Calendars.AddAsync(calendar);
+                    await context.SaveChangesAsync();
+                }
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    var repository = new CalendarRepository(context);
+
+                    await repository.DeleteAllEventsAsync(calendar.Id);
+                }
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    Assert.Single(context.Calendars);
+                    Assert.Empty(context.Events);
+                    Assert.Empty(context.EventMentions);
+                    Assert.Empty(context.EventRSVPs);
+                }
+            }
+        }
+
+        public class DeleteAsyncMethod
+        {
+            [Fact]
+            public async Task DeletesCalendar()
+            {
+                var options = new DbContextOptionsBuilder<SchedulerBotDbContext>()
+                    .UseInMemoryDatabase(databaseName: $"{GetType().DeclaringType.Name}_{GetType().Name}_{nameof(DeletesCalendar)}")
+                    .Options;
+
+                var calendar = new Calendar
+                {
+                    Id = 1
+                };
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    await context.Calendars.AddAsync(calendar);
+                    await context.SaveChangesAsync();
+                }
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    var repository = new CalendarRepository(context);
+
+                    await repository.DeleteAsync(calendar);
+                }
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    Assert.Empty(context.Calendars);
+                }
+            }
+        }
+
+        public class GetByIdAsyncMethod
+        {
+            [Fact]
+            public async Task ReturnsCalendarWithUlongId()
+            {
+                var options = new DbContextOptionsBuilder<SchedulerBotDbContext>()
+                    .UseInMemoryDatabase(databaseName: $"{GetType().DeclaringType.Name}_{GetType().Name}_{nameof(ReturnsCalendarWithUlongId)}")
+                    .Options;
+
+                var calendar = new Calendar
+                {
+                    Id = 1
+                };
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    await context.Calendars.AddAsync(calendar);
+                    await context.SaveChangesAsync();
+                }
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    var repository = new CalendarRepository(context);
+
+                    var result = await repository.GetByIdAsync(calendar.Id);
+
+                    Assert.Equal(calendar.Id, result.Id);
+                }
+            }
+        }
+
+        public class ListAllAsyncMethod
+        {
+            [Fact]
+            public async Task ReturnsCalendars()
+            {
+                var options = new DbContextOptionsBuilder<SchedulerBotDbContext>()
+                    .UseInMemoryDatabase(databaseName: $"{GetType().DeclaringType.Name}_{GetType().Name}_{nameof(ReturnsCalendars)}")
+                    .Options;
+
+                var calendars = new List<Calendar>
+                {
+                    new Calendar { Id = 1 },
+                    new Calendar { Id = 2 }
+                };
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    await context.Calendars.AddRangeAsync(calendars);
+                    await context.SaveChangesAsync();
+                }
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    var repository = new CalendarRepository(context);
+
+                    var result = await repository.ListAllAsync();
+
+                    Assert.Equal(calendars.Count, result.Count);
+                    foreach (var calendar in calendars)
+                    {
+                        Assert.Contains(result, r => r.Id == calendar.Id);
+                    }
+                }
+            }
+        }
+
+        public class UpdateAsyncMethod
+        {
+            [Fact]
+            public async Task UpdatesCalendar()
+            {
+                var options = new DbContextOptionsBuilder<SchedulerBotDbContext>()
+                    .UseInMemoryDatabase(databaseName: $"{GetType().DeclaringType.Name}_{GetType().Name}_{nameof(UpdatesCalendar)}")
+                    .Options;
+
+                var calendar = new Calendar
+                {
+                    Id = 1,
+                    Prefix = "a"
+                };
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    await context.Calendars.AddAsync(calendar);
+                    await context.SaveChangesAsync();
+                }
+
+                var newPrefix = "b";
+                calendar.Prefix = newPrefix;
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    var repository = new CalendarRepository(context);
+
+                    await repository.UpdateAsync(calendar);
+                }
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    var dbCalendar = await context.Calendars.FirstAsync();
+
+                    Assert.Equal(newPrefix, dbCalendar.Prefix);
+                }
+            }
+        }
+    }
+}
---
+++ SchedulerBot.Persistence/Repositories/CalendarRepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SchedulerBot.Application.Interfaces;
+using SchedulerBot.Data.Models;
+
+namespace SchedulerBot.Persistence.Repositories
+{
+    public class CalendarRepository : ICalendarRepository
+    {
+        private readonly SchedulerBotDbContext _context;
+
+        public CalendarRepository(SchedulerBotDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Calendar> AddAsync(Calendar entity)
+        {
+            await _context.Calendars.AddAsync(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public Task<int> CountAsync(ISpecification<Calendar> spec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteAllEventsAsync(ulong id)
+        {
+            var events= await _context.Events
+                .Where(e => e.Calendar.Id == id)
+                .ToListAsync();
+
+            var mentions = await _context.EventMentions
+                .Where(e => e.Event.Calendar.Id == id)
+                .ToListAsync();
+
+            var rsvps = await _context.EventRSVPs
+                .Where(e => e.Event.Calendar.Id == id)
+                .ToListAsync();
+
+            _context.EventMentions.RemoveRange(mentions);
+            _context.EventRSVPs.RemoveRange(rsvps);
+            _context.Events.RemoveRange(events);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Calendar entity)
+        {
+            _context.Calendars.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Calendar> GetByIdAsync(ulong id)
+        {
+            return await _context.Calendars.FindAsync(id);
+        }
+
+        public Task<Calendar> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IReadOnlyList<Calendar>> ListAllAsync()
+        {
+            return await _context.Calendars.ToListAsync();
+        }
+
+        public Task<IReadOnlyList<Calendar>> ListAsync(ISpecification<Calendar> spec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(Calendar entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
