5523f70bba4af6ea61aa10f8462fe74bd73ab940
Haryz Izzudin <haryzizzudin@gmail.com>
2019-08-31 18:45:49 +0800

    Add event repository implementation

---
+++ SchedulerBot.Persistence.UnitTests/Repositories/EventRepositoryFacts.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchedulerBot.Data.Models;
+using SchedulerBot.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SchedulerBot.Persistence.UnitTests.Repositories
+{
+    public class EventRepositoryFacts
+    {
+        public class AddAsyncMethod
+        {
+            [Fact]
+            public async Task AddsEvent()
+            {
+                var options = new DbContextOptionsBuilder<SchedulerBotDbContext>()
+                    .UseInMemoryDatabase(databaseName: $"{GetType().DeclaringType.Name}_{GetType().Name}_{nameof(AddsEvent)}")
+                    .Options;
+
+                var @event = new Event
+                {
+                    Id = Guid.NewGuid()
+                };
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    var repository = new EventRepository(context);
+
+                    var result = await repository.AddAsync(@event);
+
+                    Assert.Equal(@event.Id, result.Id);
+                }
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    Assert.Single(context.Events);
+                }
+            }
+        }
+
+        public class DeleteAsyncMethod
+        {
+            [Fact]
+            public async Task DeletesEvent()
+            {
+                var options = new DbContextOptionsBuilder<SchedulerBotDbContext>()
+                    .UseInMemoryDatabase(databaseName: $"{GetType().DeclaringType.Name}_{GetType().Name}_{nameof(DeletesEvent)}")
+                    .Options;
+
+                var @event = new Event
+                {
+                    Id = Guid.NewGuid()
+                };
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    await context.Events.AddAsync(@event);
+                    await context.SaveChangesAsync();
+                }
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    var repository = new EventRepository(context);
+
+                    await repository.DeleteAsync(@event);
+                }
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    Assert.Empty(context.Events);
+                }
+            }
+        }
+
+        public class GetByIdAsyncMethod
+        {
+            [Fact]
+            public async Task ReturnsEvent()
+            {
+                var options = new DbContextOptionsBuilder<SchedulerBotDbContext>()
+                    .UseInMemoryDatabase(databaseName: $"{GetType().DeclaringType.Name}_{GetType().Name}_{nameof(ReturnsEvent)}")
+                    .Options;
+
+                var @event = new Event
+                {
+                    Id = Guid.NewGuid()
+                };
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    await context.Events.AddAsync(@event);
+                    await context.SaveChangesAsync();
+                }
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    var repository = new EventRepository(context);
+
+                    var result = await repository.GetByIdAsync(@event.Id);
+
+                    Assert.Equal(@event.Id, result.Id);
+                }
+            }
+        }
+
+        public class ListAllAsyncMethod
+        {
+            [Fact]
+            public async Task ReturnsEvents()
+            {
+                var options = new DbContextOptionsBuilder<SchedulerBotDbContext>()
+                    .UseInMemoryDatabase(databaseName: $"{GetType().DeclaringType.Name}_{GetType().Name}_{nameof(ReturnsEvents)}")
+                    .Options;
+
+                var events = new List<Event>
+                {
+                    new Event { Id = Guid.NewGuid() },
+                    new Event { Id = Guid.NewGuid() }
+                };
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    await context.Events.AddRangeAsync(events);
+                    await context.SaveChangesAsync();
+                }
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    var repository = new EventRepository(context);
+
+                    var result = await repository.ListAllAsync();
+
+                    Assert.Equal(events.Count, result.Count);
+                    foreach (var @event in events)
+                    {
+                        Assert.Contains(result, r => r.Id == @event.Id);
+                    }
+                }
+            }
+        }
+
+        public class UpdateAsyncMethod
+        {
+            [Fact]
+            public async Task UpdatesEvent()
+            {
+                var options = new DbContextOptionsBuilder<SchedulerBotDbContext>()
+                    .UseInMemoryDatabase(databaseName: $"{GetType().DeclaringType.Name}_{GetType().Name}_{nameof(UpdatesEvent)}")
+                    .Options;
+
+                var @event = new Event
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "a"
+                };
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    await context.Events.AddAsync(@event);
+                    await context.SaveChangesAsync();
+                }
+
+                var newName = "b";
+                @event.Name = newName;
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    var repository = new EventRepository(context);
+
+                    await repository.UpdateAsync(@event);
+                }
+
+                using (var context = new SchedulerBotDbContext(options))
+                {
+                    var dbEvent = await context.Events.FirstAsync();
+
+                    Assert.Equal(newName, dbEvent.Name);
+                }
+            }
+        }
+    }
+}
---
+++ SchedulerBot.Persistence/Repositories/EventRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SchedulerBot.Application.Interfaces;
+using SchedulerBot.Data.Models;
+
+namespace SchedulerBot.Persistence.Repositories
+{
+    public class EventRepository : IEventRepository
+    {
+        private readonly SchedulerBotDbContext _context;
+
+        public EventRepository(SchedulerBotDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Event> AddAsync(Event entity)
+        {
+            await _context.Events.AddAsync(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public Task<int> CountAsync(ISpecification<Event> spec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteAsync(Event entity)
+        {
+            _context.Events.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Event> GetByIdAsync(Guid id)
+        {
+            return await _context.Events.FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<Event>> ListAllAsync()
+        {
+            return await _context.Events.ToListAsync();
+        }
+
+        public Task<IReadOnlyList<Event>> ListAsync(ISpecification<Event> spec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(Event entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
